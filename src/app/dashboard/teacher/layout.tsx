55076e421f653cddee0499267732bde88cab63a5
Kaiilil <nguyenvothanh2004@gmail.com>
2025-10-27 20:16:38 +0700

    .

--- src/app/dashboard/teacher/layout.tsx
+++ src/app/dashboard/teacher/layout.tsx
@@ -1,18 +1,16 @@
-"use client";
+'use client';
+
 import Sidebar from "@/components/teacher/Sidebar";
 import Header from "@/components/teacher/Header";
-import ProtectedRoute from "@/components/auth/ProtectedRoute";
 
-export default function DashboardLayout({ children }: { children: React.ReactNode }) {
+export default function TeacherLayout({ children }: { children: React.ReactNode }) {
   return (
-    <ProtectedRoute allowedRoles={['teacher']}>
-      <div className="flex">
-        <Sidebar />
-        <main className="ml-72 min-h-screen w-full bg-gray-50">
-          <Header />
-          {children}
-        </main>
-      </div>
-    </ProtectedRoute>
+    <div className="flex">
+      <Sidebar />
+      <main className="ml-72 min-h-screen w-full bg-gray-50">
+        <Header />
+        {children}
+      </main>
+    </div>
   );
 }
